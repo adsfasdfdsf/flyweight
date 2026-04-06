b5688ae068d843c65ef1e907472bd2f38db99d01
sadfad <alfhcwl@yandex.ru>
2026-04-06 18:32:21 +0300

    facade

--- Flyweight/Models/Order.cs
+++ Flyweight/Models/Order.cs
@@ -3,14 +3,7 @@ namespace ECommerceMVP;
 // Заказ
 public class Order
 {
-    private List<Product> _products = new List<Product>();
-    public void AddProduct(Product product)
-    {
-        _products.Add(product);
-    }
-    public decimal GetTotalAmount()
-    {
-        return _products.Sum(p => p.Price);
-    }
-    public IEnumerable<Product> Products => _products;
+    public decimal TotalAmount { get; set; }
+
+    public decimal GetTotalAmount() => TotalAmount;
 }
--- Flyweight/Program.cs
+++ Flyweight/Program.cs
@@ -10,38 +10,13 @@ namespace ECommerceMVP
     {
         static void Main(string[] args)
         {
-            // Выбор платежного шлюза (например, PayPal)
-            Console.WriteLine("Choose gateway: \n1. PayPalGateway\n2. CreditCardGateway");
-            var input = Console.ReadLine();
-            IPaymentGateway gateway = input switch
-            {
-                "1" => new PayPalGateway(),
-                "2" => new CreditCardGateway(),
-                _ => throw new NotImplementedException(),
-            };
+            Order order = new Order { TotalAmount = 2450m };
+            OrderProcessingFacade facade = new OrderProcessingFacade();
+            facade.ProcessOrder(order);
+            facade.CancelOrder(order);
             
-            Console.WriteLine("Input amount to pay (PayPal balance is 1000)");
-            PaymentProcessor processor = new OnlinePaymentProcessor(gateway);
-            input = Console.ReadLine();
-            if (input == null)
-            {
-                processor.ProcessPayment(2450m);
-                Console.ReadLine();
-                return;
-            }
-            
-            int amount = int.Parse(input);
-
-            try
-            {
-                processor.ProcessPayment(amount);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message + "\n ...Changing Strategy");
-                processor.ChangeGateway(new CreditCardGateway());
-                processor.ProcessPayment(amount);
-            }
+            order.TotalAmount = 5000m;
+            facade.ProcessOrder(order);
 
             Console.ReadLine();
         }
